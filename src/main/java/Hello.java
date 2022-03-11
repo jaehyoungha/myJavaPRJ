b573e216d530b6733846b23036d0d34bb4757575
Hajaehyoung <hjk8893@gmail.com>
2022-03-11 15:58:13 +0900

    first

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
     * */
     public  static  void main(String[] args){
 
-       System.out.println("Hello!! My First Java Coding!!");
+       System.out.println("Hei!! My First Java Coding!!");
 
     }
 }
