cd82721516c2a2cfd56efcf9bd9102895ffecdec
hajaehyeong <hajaehyeong@218.38.137.27>
2022-03-25 21:19:55 +0900

    220325

---
+++ Hello/src/Jeahyoung/Java_220325_1.java
@@ -0,0 +1,12 @@
+package Jeahyoung;
+
+public class Java_220325_1 {
+	public static void main(String[] args) {
+		System.out.printf("%d / %d = %d", 100 , 200, 0.5);
+		System.out.printf("\n"); // %d 가 아닌 %f가 들어가야함.
+		// %d = 정수 ,%f = 소수점이 있는 실수 ex) 0.5, 1.0
+		// %c = 문자 한 글자 '' 작은 따옴표
+		// %s = 문자열, 한 글자 이상 "" 큰 따옴표
+	}
+
+}
---
+++ Hello/src/Jeahyoung/Java_220325_2.java
@@ -0,0 +1,21 @@
+package Jeahyoung;
+
+public class Java_220325_2 extends Java_220325_1 { 
+public static void main (String [] args) {
+    System.out.printf("%d\n", 123);// 정수 출력
+    System.out.printf("%5d\n", 123);// 정수 5자리 확보
+    System.out.printf("%05d\n", 123);// 정수 5자리 확보, 공백 0으로 출
+    
+    System.out.printf("%f \n", 123.45);//실수 출력 
+    System.out.printf("%7.1f\n", 123.45);//실수 출력 7자리확보,소수점1의 자
+    System.out.printf("%7.3f\n", 123.45);//실수 출력 자리확보,소수점 셋째자
+    
+    System.out.printf("%s\n", "Hi~Java"); //문자열 출력
+    System.out.printf("%10s\n", "Hi~Java");//문자열 출력 10자리 확보
+    
+    
+    
+    
+}
+
+}
