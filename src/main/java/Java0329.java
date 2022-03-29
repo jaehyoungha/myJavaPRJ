096690f31b8fa334f80e9fb692d4e2956b450c71
hajaehyeong <hjk8893@gmail.com>
2022-03-29 14:06:16 +0900

    22 03 29 증감 연산자 연습

---
+++ src/main/java/Java0329.java
@@ -0,0 +1,12 @@
+public class Java0329 {
+    public static void main(String[] args) {
+        int a=7, b;
+        b = (a++) -2;
+
+        System.out.printf("%d \n", b);
+
+        b = (++a) + 2;
+
+        System.out.printf("%d \n", b);
+    }
+}
