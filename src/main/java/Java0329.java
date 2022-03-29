6670942fecb666503edcd9b97ef6b867d577a985
hajaehyoung <hjk8893@gmail.com>
2022-03-29 13:52:39 +0900

    2022 03 29 증감 연산자

---
+++ src/main/java/Java0329.java
@@ -0,0 +1,15 @@
+
+
+public class Java0329 {
+    public static void main(String[] args) {
+            int a=7, b;
+            b = (a++) - 2;
+
+              System.out.printf("%d \n", b);
+
+            b = (++a) + 2;
+
+          System.out.printf("%d \n", b);
+
+    }
+}
