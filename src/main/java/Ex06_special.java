595ce9d60bbe81d2d09023916cb042b86daa5020
hajaehyoung <hjk8893@gmail.com>
2022-04-06 11:36:42 +0900

    2022 04 06 for문 수업

---
+++ src/main/java/Ex06_special.java
@@ -0,0 +1,12 @@
+public class Ex06_special {
+    public static void main(String[] args) {
+        int f = 1;
+        int i;
+
+        for (i = 5; i > 0; i--) {
+            f *= i;
+            System.out.println("중간 계산값 :"+ f);
+        }
+        System.out.println("5부터 1까지 곱한 값 :"+ f);
+    }
+}
