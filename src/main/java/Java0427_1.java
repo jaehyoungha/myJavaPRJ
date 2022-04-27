b72b964629b8720cb9c580e52ebe1f21bb82ab7c
hajaehyoung <hjk8893@gmail.com>
2022-04-27 15:39:39 +0900

    2022 04 27 수업

---
+++ src/main/java/Java0427_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Java0427_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int [] aa = new int[23];
+
+        for (int i = 0; i <n; i++) {
+            aa[sc.nextInt()-1]++;
+        }
+        for (int i = 0; i <aa.length; i++) System.out.println(aa [i] + "");
+    }
+}
