fdd7a4bf18efebe0769d7001720aab7dfcb52043
hajaehyoung <hjk8893@gmail.com>
2022-05-02 16:02:20 +0900

    2022 05 02 배열 나머지 갯수구하기

---
+++ src/main/java/Java0502.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Java0502 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> q = new HashSet<Integer>();
+
+        for (int i = 0; i <10; i++) {
+            q.add(sc.nextInt()%42);
+        }
+        sc.close();
+        System.out.print(q.size());
+    }
+
+
+}
