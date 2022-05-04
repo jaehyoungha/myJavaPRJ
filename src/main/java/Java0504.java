d627cb33355864918f79946f771b39f021dd27ea
hajaehyoung <hjk8893@gmail.com>
2022-05-04 21:33:58 +0900

    2022 05 04 배열

---
+++ src/main/java/Java0504.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Java0504 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] b = new int[a];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = sc.nextInt();
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+    }
+}
+
+
+
