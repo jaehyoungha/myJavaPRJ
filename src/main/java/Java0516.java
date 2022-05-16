fffbe610468ed88a53b82d009e4758cc5d5bcd49
hajaehyeong <hjk8893@gmail.com>
2022-05-16 16:04:57 +0900

    2022 05 16 문자 길이 구하기

---
+++ src/main/java/Java0516.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Java0516 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String [] arr = a.split("");
+
+        System.out.println(arr.length);
+    }
+}
