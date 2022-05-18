05ff4924250edf1425af303c1ad47e3e6b7286b1
hajaehyoung <hjk8893@gmail.com>
2022-05-18 10:17:43 +0900

    2022 05 18 수업 예문 9-1

---
+++ src/main/java/Ex_9_1.java
@@ -0,0 +1,10 @@
+public class Ex_9_1 {
+    public static void main(String[] args) {
+        String str = "IT CookBook. Java";
+        int len;
+        len = str.length();
+
+        System.out.println("문자열 : "+str);
+        System.out.println("문자열길이 : "+str.length());
+    }
+}
