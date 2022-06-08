524d2e1e1d6409fd37389f475b45006eb944a9a1
hajaehyoung <hjk8893@gmail.com>
2022-06-08 10:44:31 +0900

    2022 06 08 수업 예제 10-1

---
+++ src/main/java/Class0608_ex10_1.java
@@ -0,0 +1,12 @@
+public class Class0608_ex10_1 {
+    public static void main(String[] args) {
+        int [] aa = new int[3];
+        try {
+            aa[3] = 100;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("배열 첨자가 배열 크기보다 커요 ~~");
+            System.out.println("에러 메시지 : " + e);
+
+        }
+    }
+}
