ae48510f35f3e24c605bbf55f379b1beda018a69
hajaehyeong <hjk8893@gmail.com>
2022-06-24 10:42:26 +0900

    2022 06 24 함수 호출 연습

---
+++ src/main/java/Java0624.java
@@ -0,0 +1,10 @@
+public class Java0624 {
+    public static void main(String[] args) {
+        Java0624 j = new Java0624();
+        System.out.println(j.sum(2,3));
+    }
+    public int sum(int num1,int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+}
